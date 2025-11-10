f6bc3beef5f3e2381985ff85a1b5676a50aa3dc8
timhou1111 <82932584+timhou1111@users.noreply.github.com>
2025-11-10 18:18:51 +0800

    update

--- J_How/題目/APCS_遊戲選角.py
+++ J_How/題目/APCS_遊戲選角.py
@@ -1,1 +1,18 @@
 # https://zerojudge.tw/ShowProblem?problemid=m931
+
+#你要輸入幾個角色
+rule = input()
+x = []
+power = []
+y = 0
+for i in range (int(rule)):
+    z = input()
+    z = z.split()
+    x.append(z)    
+    power.append(int(z[0])** 2 +int(z[1])** 2)
+
+power1 = power.copy()
+power1.sort()
+y = (power.index(power1[-2]))
+print (" ".join(x[y]))
+
---
+++ J_How/題目/g275七言對聯.py
@@ -0,0 +1,8 @@
+# https://zerojudge.tw/ShowProblem?problemid=g275
+
+input_num = int(input())
+
+for _ in range(input_num):
+    # 讀取兩句對聯
+    line1 = input().split()
+    line2 = input().split()
